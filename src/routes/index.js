3e2c7069471b052340e5494803216d157c684bb8
josemontesricardo <josehilariomontesricardo@gmail.com>
2016-10-20 18:48:48 -0500

    test6

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,14 +10,14 @@ class Routes {
     homeRoute(){
         console.log('holaa')
         this.home = {
-            route : app.router('/home')
+            route : this.app.router('/home')
         }
     }
 
     userRoute(){
         console.log('holaa2')        
         this.home = {
-            route : app.router('/home')
+            route : this.app.router('/home')
         }
     }
 
